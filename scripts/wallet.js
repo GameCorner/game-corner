3104d0d037395874359239df4ffa8d2cfac069d5
IceKirby <cezartoshimi@live.com>
2013-06-23 20:03:41 -0300

    Update wallet.js

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -1,11 +1,11 @@
-/*global casino, casinobot, sys, module, getKey, saveKey, sendChanAll, SESSION*/
+/*global casino, casinobot, sys, module, SESSION*/
 function Casino() {
     var wallet = this;
     
     this.enterCasino = function(src, data, chan) {
         if (this.IsInCasino(src)) {
             casinobot.sendMessage(src, "You already entered the casino!", chan);
-            return
+            return;
         }
         
         var name = sys.name(src);
@@ -15,12 +15,12 @@ function Casino() {
             user.casino = {
                 name: name,
                 coins: parseInt(key, 10)
-            }
+            };
         } else {
             user.casino = {
                 name: name,
                 coins: 1000
-            }
+            };
         }
         
         sys.saveVal("Casino", name.toLowerCase(), user.casino.coins);
@@ -42,20 +42,47 @@ function Casino() {
         sys.saveVal("Casino", user.name.toLowerCase(), user.coins);
         return user.coins;
     };
+    this.giveCoins = function(src, data, chan) {
+        var info = data.split(":");
+        if (info.length < 2) {
+            casinobot.sendMessage(src, "Invalid format! Use /givecoins [name]:[coins].", chan);
+            return;
+        }
+        
+        var target = sys.id(info[0]);
+        if (target === undefined || this.IsInCasino(target) === false) {
+            casinobot.sendMessage(src, "This person is not in the Casino!", chan);
+            return;
+        }
+        var amount = parseInt(info[1], 10);
+        if (isNaN(amount)) {
+            casinobot.sendMessage(src, "Invalid value! You must specify a valid integer!", chan);
+            return;
+        }
+        
+        this.addCoins(target, amount);
+        if (amount > 0) {
+            casinobot.sendMessage(src, "You gave " + amount + " coins to " + sys.name(target) + "!", chan);
+            casinobot.sendMessage(target, "You received " + amount + " coins from " + sys.name(src) + "!");
+        } else if (amount < 0) {
+            casinobot.sendMessage(src, "You took " + (-amount) + " coins from " + sys.name(target) + "!", chan);
+            casinobot.sendMessage(target, sys.name(src) + " took " + (-amount) + " coins from you!");
+        }
+    };
     this.viewCoins = function(src, data, chan) {
         if (this.IsInCasino(src)) {
             casinobot.sendMessage(src, "You currently have " + SESSION.users(src).casino.coins + " Coin(s)!", chan);
         } else {
             casinobot.sendMessage(src, "You are not in the casino! Type /entercasino to join!", chan);
         }
-    }
+    };
     
     this.IsInCasino = function(src) {
         return SESSION.users(src).hasOwnProperty("casino") && SESSION.users(src).casino !== null;
     };
     
     this.init = function() {
-    	setWallet();
+		setWallet();
     };
     
     function setWallet() {
@@ -94,8 +121,11 @@ function Casino() {
         if (command === "entercasino") {
             this.enterCasino(src, data, chan);
             return true;
-        } else if (command === "coins") {
+        } else if (command === "coins" || command === "mycoins") {
             this.viewCoins(src, data, chan);
+            return true;
+        } else if (command === "givecoins" && sys.auth(src) >= 2) {
+            this.giveCoins(src, data, chan);
             return true;
         }
         throw("No valid command");
