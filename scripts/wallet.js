fde09f0f34aa788c36e16a20d8c64ac12eb8d93f
IceKirby <cezartoshimi@live.com>
2013-06-22 22:14:09 -0300

    Create wallet.js

---
+++ scripts/wallet.js
@@ -0,0 +1,107 @@
+/*global casino, casinobot, sys, module, getKey, saveKey, sendChanAll, SESSION*/
+function Casino() {
+    var casino = this;
+    this.enterCasino = function(src, data, chan) {
+        if (this.IsInCasino(src)) {
+            casinobot.sendMessage(src, "You already entered the casino!", chan);
+            return
+        }
+        
+        var name = sys.name(src);
+        var user = SESSION.users(src);
+        var key = getKey("casino", name.toLowerCase());
+        if (key) {
+            user.casino = {
+                name: name,
+                coins: parseInt(key, 10)
+            }
+        } else {
+            user.casino = {
+                name: name,
+                coins: 1000
+            }
+        }
+        
+        saveKey("casino", name.toLowerCase(), user.casino.coins);
+        casinobot.sendMessage(src, "You have " + user.casino.coins + " Coins!", chan);
+    };
+    
+    this.addCoins = function(src, amount) {
+        if (!this.IsInCasino(src)) {
+            return 0;
+        }
+        
+        var user = SESSION.users(src).casino;
+        user.coins += amount;
+        if (user.coins < 0) {
+            user.coins = 0;
+        }
+        
+        saveKey("casino", user.name.toLowerCase(), user.coins);
+        return user.coins;
+    };
+    this.viewCoins = function(src, data, chan) {
+        if (this.IsInCasino(src)) {
+            casinobot.sendMessage(src, "You currently have " + SESSION.users(src).casino.coins + " Coins!", chan);
+        } else {
+            casinobot.sendMessage(src, "You are not in the casino! Type /entercasino to join!", chan);
+        }
+    }
+    
+    this.IsInCasino = function(src) {
+        return SESSION.users(src).hasOwnProperty("casino") && SESSION.users(src).casino !== null;
+    };
+    
+    this.init = function() {
+    	
+    };
+    this.handleCommand = function(src, message, channel) {
+        var command;
+		var commandData = '*';
+		var pos = message.indexOf(' ');
+		if (pos !== -1) {
+			command = message.substring(0, pos).toLowerCase();
+			commandData = message.substr(pos+1);
+		} else {
+			command = message.substr(0).toLowerCase();
+		}
+        
+        try {
+			casino.handleWalletCommand(src, command, commandData, channel);
+            return true;
+        } catch(e) {
+            if (e !== "No valid command") {
+                sys.sendAll("Error on Casino command" + (e.lineNumber ? " on line " + e.lineNumber : "") + ": " + e, channel);
+                return true;
+            }
+        }
+    };
+    this.handleWalletCommand = function(src, command, data, chan) {
+        var name = sys.name(src);
+        
+        if (command === "entercasino") {
+            this.enterCasino(src, data, chan);
+            return true;
+        } else if (command === "coins") {
+            this.viewCoins(src, data, chan);
+            return true;
+        }
+        throw("No valid command");
+    };
+}
+
+module.exports = function() {
+    var init = function() {
+        
+    };
+
+    var game = new Casino();
+    
+    return {
+        casino: game,
+        init: game.init,
+        handleCommand: game.handleCommand,
+        addCoins: game.addCoins,
+        IsInCasino: game.IsInCasino
+    };
+}();
